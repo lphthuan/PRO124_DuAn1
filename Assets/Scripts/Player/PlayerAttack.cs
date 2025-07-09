b0ef4d6668bfdebeb912ce9c60be74766be4d498
Le Phan Hoa Thuan <lephanhoathuan2006@gmail.com>
2025-07-09 20:27:16 +0700

    [Update] light spell

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+	[SerializeField] private GameObject spellPrefab;
+	[SerializeField] private Transform firePoint;
+	[SerializeField] private float spellSpeed = 10f;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0)) // Chuột trái
+		{
+			ShootSpell();
+		}
+	}
+
+	void ShootSpell()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = (mousePos - firePoint.position).normalized;
+
+		GameObject spell = Instantiate(spellPrefab, firePoint.position, Quaternion.identity);
+		Rigidbody2D rb = spell.GetComponent<Rigidbody2D>();
+		rb.velocity = direction * spellSpeed;
+	}
+}
