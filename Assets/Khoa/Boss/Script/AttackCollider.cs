203777ba0d02851f792a193490ae911dedc23f30
khoatnd223 <yodayo0907@gmail.com>
2025-07-10 12:08:51 +0700

    UpdateBossAtk
    chuyển động mượt hơn, đã có atk, chưa set tag

---
+++ Assets/Khoa/Boss/Script/AttackCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private BossMovement bossScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossScript = GetComponentInParent<BossMovement>();
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Gọi hàm từ script cha (boss) khi va chạm
+            bossScript.OnHitPlayer(other.gameObject);
+        }
+    }
+}
