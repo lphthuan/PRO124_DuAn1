e17ea46d466bc9abd3d4980b476897495b46fc3a
khoatnd223 <yodayo0907@gmail.com>
2025-07-11 21:34:05 +0700

    updateSkill1Boss-NotDone
    vẫn còn lỗi xung đột giữa 2 trạng thái animation

--- Assets/Khoa/Boss/Script/MagicAreaCheck.cs
+++ Assets/Khoa/Boss/Script/MagicAreaCheck.cs
@@ -14,7 +14,7 @@ public class MagicAreaCheck : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
+            bossScript.startSpawnMagicAreaDmg();
         }
     }
 }
