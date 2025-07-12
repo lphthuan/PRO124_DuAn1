b09584c0aa71cb5dc40a48b94d50371b4b525aa0
NguyenTriTin3008 <tritin.caolanh@gmail.com>
2025-07-12 20:57:51 +0700

    Loading

--- Assets/Tin/Script/MenuManager.cs
+++ Assets/Tin/Script/MenuManager.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(1);
-    }
-
     public void Quit()
     {
         Application.Quit();
