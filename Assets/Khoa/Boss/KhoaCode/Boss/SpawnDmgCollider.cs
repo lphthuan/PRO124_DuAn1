b7f769cce242d9d30dced24044cc1cad15c625d3
khoatnd223 <yodayo0907@gmail.com>
2025-07-14 16:48:27 +0700

    UpdateTruocKhiThuyetTrinh

--- Assets/Khoa/Boss/KhoaCode/Boss/SpawnDmgCollider.cs
+++ Assets/Khoa/Boss/KhoaCode/Boss/SpawnDmgCollider.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnDmgCollider : MonoBehaviour
 {
-    private float toaDoY = -0.3f;
+    private float toaDoY = -0.5f;
     [SerializeField] GameObject MagicDmgArea;
     public void SpawnDamageCollider()
     {
