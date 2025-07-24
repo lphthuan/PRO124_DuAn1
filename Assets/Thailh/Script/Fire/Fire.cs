d0e78b212c7cea3d6a36df2a3c80e77cde9eb390
lethai-02 <thailhpc07573@fpt.edu.vn>
2025-07-24 13:55:50 +0700

    quai 4 fire
    tam xong

---
+++ Assets/Thailh/Script/Fire/Fire.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public int damage = 5;
+    public float lifeTime = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime); // Tự hủy sau 5 giây (tránh tồn tại vô hạn)
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth health = other.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+              
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
