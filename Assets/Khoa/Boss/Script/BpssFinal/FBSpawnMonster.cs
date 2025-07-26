0155abcf46321c2cab1241870663735a1b8695a2
khoatnd223 <yodayo0907@gmail.com>
2025-07-26 12:15:11 +0700

    UpdateSkillSummonFinalBoss

---
+++ Assets/Khoa/Boss/Script/BpssFinal/FBSpawnMonster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBSpawnMonster : MonoBehaviour
+{
+    private float toaDoY = -1f;
+    [SerializeField] GameObject SpawnMonster;
+    public void SpawnModCollider()
+    {
+        Vector3 spawnMonster = transform.position;
+        spawnMonster.y += toaDoY;
+        GameObject monster = Instantiate(SpawnMonster, spawnMonster, Quaternion.identity);
+    }
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
+
--- Assets/Khoa/Boss/Script/BpssFinal/FinalBoss.cs
+++ Assets/Khoa/Boss/Script/BpssFinal/FinalBoss.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,23 +6,61 @@ public class FinalBoss : MonoBehaviour
 {
     public Animator animator;
     public int phaseFinalBoss = 0;
-    [SerializeField] GameObject monster;
+    [SerializeField] GameObject castSkill;
+    [SerializeField] GameObject spawnAreaMonster;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void phase1BossFinal()
     {
         animator.SetBool("Phase1", true);
     }
+    public void CastSkillSummonMagic1()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x - 7.5f, transform.position.y + 4f, transform.position.z);
+        GameObject SkillSummonMagic1 = Instantiate(castSkill, spawnPos, Quaternion.identity);
+        Destroy(SkillSummonMagic1, 1.7f);
+    }
+    public void CastSkillSummonMagic2()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x + 7.5f, transform.position.y + 4f, transform.position.z);
+        GameObject SkillSummonMagic2 = Instantiate(castSkill, spawnPos, Quaternion.identity);
+        Destroy(SkillSummonMagic2, 1.7f);
+    }
+    public void spawnMonsterArea1()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x - 15f, transform.position.y - 5.5f, transform.position.z);
+        GameObject spawnMonsterArea1 = Instantiate(spawnAreaMonster, spawnPos, Quaternion.identity);
+        Destroy(spawnMonsterArea1, 5f);
+    }
 
-   
+    public void spawnMonsterArea2()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x + 15f, transform.position.y - 5.5f, transform.position.z);
+        GameObject spawnMonsterArea2 = Instantiate(spawnAreaMonster, spawnPos, Quaternion.identity);
+        Destroy(spawnMonsterArea2, 5f);
+    }
 
+
+
+
+
+    private IEnumerator castSkillPhase1()
+    {
+        CastSkillSummonMagic1();
+        yield return new WaitForSeconds(2f);
+        CastSkillSummonMagic2();
+        yield return new WaitForSeconds(2f);
+        spawnMonsterArea1();
+        yield return new WaitForSeconds(2f);
+        spawnMonsterArea2();
+    }
 }
