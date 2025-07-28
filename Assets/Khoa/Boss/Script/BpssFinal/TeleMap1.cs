968c2b036773fcc0b5c7d2f9c10ca0e533ee8880
khoatnd223 <yodayo0907@gmail.com>
2025-07-28 16:48:56 +0700

    update teleport zone

---
+++ Assets/Khoa/Boss/Script/BpssFinal/TeleMap1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleMap1 : MonoBehaviour
+{
+    private BossMovement bossMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossMovement = GetComponent<BossMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Vector3 teleportPosition = new Vector3(252.32f, -3.41f, 0f);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            other.transform.position = teleportPosition;
+        }
+    }
+}
