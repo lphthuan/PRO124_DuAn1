40a00211f0edb380c8910f8b1340aab026dfb41b
khoatnd223 <yodayo0907@gmail.com>
2025-07-31 15:27:35 +0700

    Update Tele chuyen scenes

--- Assets/Khoa/Boss/Script/BpssFinal/destroyBossZone.cs
+++ Assets/Khoa/Boss/Script/BpssFinal/destroyBossZone.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class destroyBossZone : MonoBehaviour
 {
+    [SerializeField] GameObject vortex;
+    public void summonVortex()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y + 5f, transform.position.z);
+        Instantiate(vortex, spawnPos, Quaternion.identity);
+    }
     public void OnDestroy()
     {
+        
         Destroy(gameObject);
     }
 }
