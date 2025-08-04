b12cf0d50170d3f1b7cd80f96e67d62ef4845ad2
khoatnd223 <yodayo0907@gmail.com>
2025-08-04 14:36:45 +0700

    MasterBoss

---
+++ Assets/Khoa/Boss/Master/MasterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterController : MonoBehaviour
+{
+    [SerializeField] GameObject MasterHit;
+
+
+
+    public void MasterAttack()
+    {
+        Vector3 spawnPos1 = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
+        GameObject SummonArrow1 = Instantiate(MasterHit, spawnPos1, Quaternion.identity);
+        Destroy(SummonArrow1, 0.2f);
+    }
+}
---
+++ Assets/Khoa/Boss/Master/MasterHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Shield"))
+        {
+            // Tìm PlayerController từ object có Shield
+            PlayerController playerController = collision.GetComponent<shieldCode>()?.playerController;
+            if (playerController != null)
+            {
+                playerController.shieldCheck = false;
+                Debug.Log("Đã tắt khiên từ Master Attack!");
+            }
+        }
+    }
+}
